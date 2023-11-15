b5e48476852f03438e26ed08abbeec2365442585
weboffools <eliot@weboffools.com>
2023-11-15 07:04:37 -0700

    Add div to all elementClass in main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { format } from 'date-fns';
 import { elementClass } from './helpers.js';
 
 export default function mainSection() {
-  const headingMain = elementClass('main-heading');
-  const headingTitle = elementClass('main-heading-title');
+  const headingMain = elementClass('div', 'main-heading');
+  const headingTitle = elementClass('div', 'main-heading-title');
   headingTitle.textContent = 'Today';
-  const headingDate = elementClass('main-heading-date');
+  const headingDate = elementClass('div', 'main-heading-date');
   headingDate.textContent = format(new Date(), 'EE, MMM io');
 
   headingMain.appendChild(headingTitle);
