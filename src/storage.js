d6682c6ee6807d29442ecef0186964de304dc2e8
weboffools <eliot@weboffools.com>
2023-11-16 18:25:06 -0700

    Add initStore to storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,18 +1,29 @@
+import Project from './project.js';
+
 function ManageStorage() {
+  
+  const currentStore = Object.entries(localStorage);
+
+  const getStore = () => currentStore;
+
+  function initStore() {
+    if (!localStorage) {
+      localStorage.setItem('projects', Project('Home'));
+    }
+  }
 
   function storagePush(task, project) {
     localStorage.setItem(project.id, JSON.stringify(task));
   }
 
   function storageLookup(searchString, type) {
-    let entries = Object.entries(localStorage);
-    let map = entries
+    let map = getStore()
       .map((entry) => JSON.parse(entry[1]))
       .filter((obj) => obj[type] === searchString);
     console.log(map);
   }
 
-  return { storagePush, storageLookup };
+  return { initStore, storagePush, storageLookup, getStore };
 }
 
 export default ManageStorage;
