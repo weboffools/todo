00310b53506e1b522cf70becbeeb80b58d012917
weboffools <eliot@weboffools.com>
2023-11-16 18:12:52 -0700

    Organize storage functions in ManageStorage FF

--- src/storage.js
+++ src/storage.js
@@ -1,23 +1,18 @@
-import { v4 as uuidv4 } from 'uuid';
-import Task from './task.js';
+function ManageStorage() {
 
-function storagePush(e) {
-  const id = uuidv4();
-  let task = Task(
-    e.target.task_name.value,
-    e.target.task_descr.value,
-    e.target.due_date.value,
-    e.target.priority.value,
-  );
-  localStorage.setItem(id, JSON.stringify(task));
+  function storagePush(task, project) {
+    localStorage.setItem(project.id, JSON.stringify(task));
+  }
+
+  function storageLookup(searchString, type) {
+    let entries = Object.entries(localStorage);
+    let map = entries
+      .map((entry) => JSON.parse(entry[1]))
+      .filter((obj) => obj[type] === searchString);
+    console.log(map);
+  }
+
+  return { storagePush, storageLookup };
 }
 
-function storageLookup() {
-  let entries = Object.entries(localStorage);
-  let search = 'Laundry';
-  let type = 'name';
-  let map = entries.map((entry) => JSON.parse(entry[1])).filter(obj => obj[type] === search);
-  console.log(map);
-}
-
-export { storagePush, storageLookup };
+export default ManageStorage;
